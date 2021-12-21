b37e4f0e6ecab139e5c9fb67e456afc6a2a46c90
Crow314 <master@crow31415.net>
2021-12-22 02:22:52 +0900

    create process which simply repeating packet

--- pkg/repeater/repeater.go
+++ pkg/repeater/repeater.go
@@ -3,5 +3,14 @@ package repeater
 import "github.com/Crow314/im920s-controller/pkg/module"
 
 func Run(im920s *module.Im920s) {
+	go runner(im920s)
+}
 
+func runner(im920s *module.Im920s) {
+	receiver := im920s.DataReceiver()
+
+	for {
+		data := <-receiver
+		_ = im920s.Broadcast(data.Data())
+	}
 }
