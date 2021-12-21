8cc142f01b86b9b17efc1d7473680b89622eb826
Crow314 <master@crow31415.net>
2021-12-22 04:44:14 +0900

    create process to check if it's already received before resend packet

--- pkg/repeater/repeater.go
+++ pkg/repeater/repeater.go
@@ -2,15 +2,23 @@ package repeater
 
 import "github.com/Crow314/im920s-controller/pkg/module"
 
-func Run(im920s *module.Im920s) {
-	go runner(im920s)
+func Run(im920s *module.Im920s, storeSize int) {
+	go runner(im920s, storeSize)
 }
 
-func runner(im920s *module.Im920s) {
+func runner(im920s *module.Im920s, storeSize int) {
 	receiver := im920s.DataReceiver()
+	store := newIDStore(storeSize)
 
 	for {
 		data := <-receiver
+
+		// 受信未済チェック
+		if store.checkExistence(data.Data()) {
+			continue
+		}
+
+		store.add(data.Data())
 		_ = im920s.Broadcast(data.Data())
 	}
 }
